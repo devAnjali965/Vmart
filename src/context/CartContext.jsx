4362747ae12e8e40bdfe440bec4d1afcad10985c
devAnjali965 <devanjali965@gmail.com>
2025-04-11 14:03:06 +0530

    Update CartContext.jsx

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,55 +1,6 @@
-// import React, { createContext, useContext, useReducer } from 'react';
-
-// const CartContext = createContext();
-
-// const cartReducer = (state, action) => {
-//   switch (action.type) {
-//     case 'ADD_TO_CART':
-//       const existingItem = state.find(item => item.id === action.payload.id);
-//       if (existingItem) {
-//         return state.map(item =>
-//           item.id === action.payload.id ? { ...item, qty: item.qty + 1 } : item
-//         );
-//       }
-//       return [...state, { ...action.payload, qty: 1 }];
-//     default:
-//       return state;
-//   }
-// };
-
-// export const CartProvider = ({ children }) => {
-//   const [cart, dispatch] = useReducer(cartReducer, []);
-
-//   return (
-//     <CartContext.Provider value={{ cart, dispatch }}>
-//       {children}
-//     </CartContext.Provider>
-//   );
-// };
-
-// export const useCart = () => useContext(CartContext);
-
 import React, { createContext, useContext, useReducer } from 'react';
 
 const CartContext = createContext();
-
-// const cartReducer = (state, action) => {
-//   switch (action.type) {
-//     case 'ADD_TO_CART':
-//       const existingItem = state.find(item => item.id === action.payload.id);
-//       if (existingItem) {
-//         return state.map(item =>
-//           item.id === action.payload.id ? { ...item, qty: item.qty + 1 } : item
-//         );
-//       }
-//       return [...state, { ...action.payload, qty: 1 }];
-
-     
-//     default:
-//       return state;
-//   }
-// };
-
 const cartReducer = (state, action) => {
     switch (action.type) {
       case 'ADD_TO_CART':
